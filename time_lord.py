d092e5a746093370fe9222e73785b20dc7f75cdb
Nicci Tynen <ntynen@gmail.com>
2023-11-28 16:48:09 -0800

    Added brief description of class Added check to see if we're using adafruit_logger library Added support for projects not using RTC, rtc will only be used if it is of type RTC All methods use a private method called provide_time_data - this allows support for using RTC or not Removed mqtt from print_message; not needed

--- time_lord.py
+++ time_lord.py
@@ -1,8 +1,14 @@
 # SPDX-License-Identifier: MIT
 
+# Wrapper class for setting and getting time information
+# Currently does not support portal boards using Network
+
 import time
 import adafruit_ntp
+from rtc import RTC
+
 import local_logger as logger
+import local_mqtt
 
 try:
     from data import data
@@ -14,11 +20,13 @@ except ImportError:
 # The time singleton
 # No value until configured
 time_lord = None
-# Assume we're not using the logging library
+# Get whether to use adafruit_logger from the data file
 use_log = data["local_logger"]
 
 
 # Configure the time singleton
+# If not using a real time clock, pass in any value for rtc
+# rtc will only get used if it is of type RTC
 def configure_time(socket_pool, rtc):
     _add_time_lord(socket_pool, rtc)
     return time_lord
@@ -30,7 +38,12 @@ def _add_time_lord(socketpool, rtc):
 
     if time_lord is None:
         time_lord = TimeLord(socketpool, rtc)
-        time_lord.set_system_clock()
+        if use_log == 1:
+            time_lord.my_log = logger.getLocalLogger()
+        if time_lord.rtc is not None:
+            time_lord.set_system_clock()
+        else:
+            time_lord.print_message("Time Singleton Created", "info")
 
 
 # Return the configured time singleton
@@ -47,23 +60,24 @@ class TimeLord:
 
     # Initialize the time singleton
     def __init__(self, socketpool, rtc):
-        self.rtc = rtc
+        if type(rtc) is not RTC:
+            self.rtc = None
+        else:
+            self.rtc = rtc
         self.socket_pool = socketpool
         self.my_log = None
+        self.ntp_client = adafruit_ntp.NTP(self.socket_pool, tz_offset=float(data["tz_offset"]))
 
     # Set up NTP client
     # get the current datetime and set the RTC
     def set_system_clock(self):
-        if use_log == 1:
-            self.my_log = logger.getLocalLogger()
 
-        ntp_client = adafruit_ntp.NTP(self.socket_pool, tz_offset=float(data["tz_offset"]))
         attempt = 0
         connect_success = False
 
         while connect_success is False:
             try:
-                self.rtc.datetime = ntp_client.datetime
+                self.rtc.datetime = self.ntp_client.datetime
                 connect_success = True
                 message = "Created Time Singleton and updated RTC"
                 self.print_message(message, "info")
@@ -82,7 +96,7 @@ class TimeLord:
     # Return the current date/time for logging
     # Formatted: Y.D.M HH:MM:SS
     def get_logging_datetime(self):
-        now = self.rtc.datetime
+        now = self._provide_time_data()
         t = "{:02d}.{:02d}.{:02d} {:02d}:{:02d}:{:02d}".format(now.tm_year, now.tm_mday, now.tm_mon, now.tm_hour,
                                                                now.tm_min, now.tm_sec)
         return t
@@ -90,7 +104,7 @@ class TimeLord:
     # Return the current date
     # Formatted: YDM
     def get_date(self, separator=None):
-        now = self.rtc.datetime
+        now = self._provide_time_data()
         if separator is not None:
             d = "{:02d}" + separator + "{:02d}" + separator + "{:02d}".format(now.tm_year, now.tm_mday, now.tm_mon)
         else:
@@ -101,27 +115,34 @@ class TimeLord:
     # Return the current time with seconds
     # Formatted: HH:MM:SS
     def get_time_seconds(self):
-        now = self.rtc.datetime
+        now = self._provide_time_data()
         t = "{:02d}:{:02d}:{:02d}".format(now.tm_hour, now.tm_min, now.tm_sec)
         return t
 
     # Return the current time without seconds
     # Formatted: HH:MM
     def get_time(self):
-        now = self.rtc.datetime
+        now = self._provide_time_data()
         t = "{:02d}:{:02d}".format(now.tm_hour, now.tm_min)
         return t
 
-    # Returns current datetime from the RTC
+    # Roll your own datetime format!
     # This method returns as a time.struct_time
-    # Format (tm_year, tm_mon, tm_mday, tm_hour, tm_min, tm_sec, tm_wday, tm_yday, tm_isdst)
+    #       Format (tm_year, tm_mon, tm_mday, tm_hour, tm_min, tm_sec, tm_wday, tm_yday, tm_isdst)
+    # Use this method if you don't want the default date display of YYYY.DD.MM
     def get_current_time(self):
-        return self.rtc.datetime
+        return self._provide_time_data()
+
+    def _provide_time_data(self):
+        if self.rtc is None:
+            return self.ntp_client.datetime
+        else:
+            return self.rtc.datetime
 
     # In order to be flexible and not create a dependency between time_lord and local_logger
     # Handle print statements accordingly
-    def print_message(self, message, level, mqtt: bool = False, sdcard_dump: bool = False):
+    def print_message(self, message, level, sdcard_dump: bool = False):
         if use_log == 1:
-            self.my_log.log_message(message, level, mqtt=mqtt, sdcard_dump=sdcard_dump)
+            self.my_log.log_message(message, level, sdcard_dump=sdcard_dump)
         else:
             print(message)
