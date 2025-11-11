663b2db166b0e59a7b1b4a3689dc7a1e3c5cca1d
niccit <ntynen+github@gmail.com>
2025-11-10 17:13:52 -1000

    added month name method to calendar_helper added format time and get unix time helpers to time_helper

--- calendar_time_helpers/calendar_helper.py
+++ calendar_time_helpers/calendar_helper.py
@@ -11,3 +11,33 @@ def get_date_from_system():
     current_date = now.strftime("%d")
     publish_date = now.strftime("%A %d %b %Y")
     return current_date, publish_date
+
+# Convert provided month in numerals to the fully qualified month name
+def get_month_name(month):
+    pub_month = None
+    if month == "12":
+        pub_month = "Dec"
+    if month == "11":
+        pub_month = "Nov"
+    if month == "10":
+        pub_month = "Oct"
+    if month == "09":
+        pub_month = "Sept"
+    if month == "08":
+        pub_month = "Aug"
+    if month == "07":
+        pub_month = "Jul"
+    if month == "06":
+        pub_month = "Jun"
+    if month == "05":
+        pub_month = "May"
+    if month == "04":
+        pub_month = "Apr"
+    if month == "03":
+        pub_month = "Mar"
+    if month == "02":
+        pub_month = "Feb"
+    if month == "01":
+        pub_month = "Jan"
+
+    return pub_month
