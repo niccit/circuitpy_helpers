e775c4579618a05ee9e7cbd232287310ee0eb439
niccit <ntynen+github@gmail.com>
2025-11-13 16:05:38 -1000

    updated sample data file fixed irregular settings in animations.json

--- led_animations/sample_data.py
+++ led_animations/sample_data.py
@@ -8,13 +8,28 @@ data = {
     ,'custom_sparkle_color': 'purple'
     ,'custom_blink_color': 'aqua'
     ,'multi_comet_color': 'palette_fall'
+    ,'custom_comet_color': 'teal'
     ,'custom_chase_color': 'gold'
     ,'multi_chase_color': ['yellow', 'blue', 'orange']
     ,'cycle_color': 'palette_christmas'
+    ,'custom_pacman_color': 'green'
     ,'custom_solid_color': 'magenta'
     ,'motion_solid_color': 'old_lace'
-    ,'rainbow_sparkle_sparkles_override': 50
-    ,'random_sparkle_speed_override': 0.05
-    ,'rainbow_cycle_speed_override': 10
-    ,'cycle_speed_override': 8
+    ,'sparkles_override': 30
+    ,'sparkle_speed_override': 0.1
+    ,'breathe_speed_override': 1
+    ,'chase_speed_override': 0.01
+    ,'blink_speed_override': 1
+    ,'cycle_speed_override': 1
+    ,'comet_speed_override': 0.001
+    ,'pacman_speed_override': 0.1
+    ,'rainbow_speed_override': 0.1
+    ,'tail_length_override': 30
+    ,'comet_reverse_override': True
+    ,'chase_reverse_override': True
+    ,'size_override': 5
+    ,'spacing_override': 6
+    ,'rate_override': 0.08
+    ,'step_override': 0.04
+    ,'count_override': 5
 }
