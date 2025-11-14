bf7bb64aaccf62fb72beb6258d493be020f2df09
niccit <ntynen+github@gmail.com>
2025-11-13 15:36:55 -1000

    Added sample data file changed tail_length to a more reasonable size

---
+++ led_animations/sample_data.py
@@ -0,0 +1,20 @@
+data = {
+    'animations': ['custom_breathe']
+    ,'num_pixels' : 300
+    ,'brightness_high' : 0.5
+    ,'brightness_low' : 0.025
+    ,'animation_wait' : 30
+    ,'custom_breathe_color': 'palette_rainbow'
+    ,'custom_sparkle_color': 'purple'
+    ,'custom_blink_color': 'aqua'
+    ,'multi_comet_color': 'palette_fall'
+    ,'custom_chase_color': 'gold'
+    ,'multi_chase_color': ['yellow', 'blue', 'orange']
+    ,'cycle_color': 'palette_christmas'
+    ,'custom_solid_color': 'magenta'
+    ,'motion_solid_color': 'old_lace'
+    ,'rainbow_sparkle_sparkles_override': 50
+    ,'random_sparkle_speed_override': 0.05
+    ,'rainbow_cycle_speed_override': 10
+    ,'cycle_speed_override': 8
+}
