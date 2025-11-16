649b2a3b83b859b968cae3051f951a59b93ff9b1
niccit <ntynen+github@gmail.com>
2025-11-16 13:36:19 -1000

    relocated datetime imports to hopefully allow for use on all boards

--- calendar_time_helpers/time_helper.py
+++ calendar_time_helpers/time_helper.py
@@ -1,13 +1,12 @@
 # SPDX-License-Identifier: MIT
 
-from datetime import datetime
-from datetime import timezone
 import time
 
 # Return the current minute and the current_time
 # Example use for current_min - know when to update a time display
 # This is for a board that has a system clock, i.e., Raspberrypi
 def get_current_time():
+    from datetime import datetime
     now = datetime.now()
     current_min = now.minute
     current_time = now.strftime("%H:%M")
@@ -21,6 +20,8 @@ def format_time(timestamp):
 
 # Will return the UNIX time
 def get_unix_time():
+    from datetime import datetime
+    from datetime import timezone
     return datetime.now(tz=timezone.utc).isoformat()
 
 # Given a timestamp, will take the hour and minute and convert it to seconds
