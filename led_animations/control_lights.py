bf69a0ecba673a6ab0e6d230c0a3673753516aca
niccit <ntynen+github@gmail.com>
2025-11-17 17:23:56 -1000

    Added new LED helper for pausing light start up until a set time and sleeping lights for a certain period of time based on pre-defined times.

---
+++ led_animations/control_lights.py
@@ -0,0 +1,25 @@
+# SPDX-License-Identifier: MIT
+import alarm
+import time
+
+# If start up time is before sunset, sleep until set_time
+def sleep_before_set_time(now_time, set_time, before_set_time, pixels):
+    if now_time < set_time:
+        time_diff = (set_time - now_time) - before_set_time
+        if 0 < time_diff <= before_set_time:
+            wake_alarm = alarm.time.TimeAlarm(monotonic_time=time.monotonic() + time_diff)
+            _blank_all(pixels)
+            alarm.light_sleep_until_alarms(wake_alarm)
+
+# If up and running, and it's shut off time, sleep until next day around set_time
+def shutdown(now_time, set_time, sleep_time, before_set_time, pixels):
+    running_time = (now_time - set_time)
+    time_diff = (sleep_time - running_time) - before_set_time
+    if time_diff > 0:
+        wake_alarm = alarm.time.TimeAlarm(monotonic_time=time.monotonic() + time_diff)
+        _blank_all(pixels)
+        alarm.light_sleep_until_alarms(wake_alarm)
+
+def _blank_all(pixels):
+   pixels.fill((0, 0, 0))
+   pixels.show()
