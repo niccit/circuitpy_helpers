567c12f2d70395bf632fcfdaf5784bfa3d6f44ac
niccit <ntynen+github@gmail.com>
2025-11-22 16:41:47 -1000

    added palettes and swapped out one color for another (chocolate for dark turquoise

--- color_helpers/getColors.py
+++ color_helpers/getColors.py
@@ -47,8 +47,8 @@ def get_color_tuple(name):
         color = adafruit_led_animation.color.OLD_LACE
     elif name == "indigo":
         color = (75,0,130)
-    elif name == "chocolate":
-        color = (210,105,30)
+    elif name == "dark turquoise":
+        color = (0,206,209)
     return color
 
 # Generate a random coloGr
@@ -71,6 +71,24 @@ def get_color_palette(name):
         palette = (adafruit_led_animation.color.RED, adafruit_led_animation.color.GREEN, adafruit_led_animation.color.YELLOW, adafruit_led_animation.color.BLUE)
     elif name == "fall":
         palette = (adafruit_led_animation.color.ORANGE, adafruit_led_animation.color.RED, adafruit_led_animation.color.GOLD, adafruit_led_animation.color.YELLOW)
+    elif name == "hanukkah":
+        palette = (adafruit_led_animation.color.BLUE, (240, 240, 240))
+    elif name == "halloween":
+        palette = ((247,95,28),(255,154,0), (136,30,228), (133,226,31))
+    elif name == "patrick":
+        palette = ((34,77,23), (9,148,65), (96,168,48), (159,218,64), (217,233,29))
+    elif name == "valentine":
+        palette = ((94,8,30), (181,26,58), (226,71,103), (228,131,151), (228,205,211))
+    elif name == "easter":
+        palette = ((255,212,229), (224,205,255), (189,232,239), (183,215,132), (254,244,162))
+    elif name == "fourth":
+        palette = ((30,24,96), (196,31,64), (240, 240, 240))
+    elif name == "blues":
+        palette = (adafruit_led_animation.color.BLUE, (24, 24, 112), (0, 0, 128), (65,105,255), (245, 24, 112))
+    elif name == "purples":
+        palette = (adafruit_led_animation.color.PURPLE, adafruit_led_animation.color.MAGENTA, (218,112,214), (153,50,204))
+    elif name == "sea":
+        palette = (adafruit_led_animation.color.AQUA, adafruit_led_animation.color.TEAL, (127,255,212), (32,178,170), (60, 179.113))
     return palette
 
 
