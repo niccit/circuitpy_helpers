e08e4541a9f287b6c7f2c6a54fce3bdbdbda6b83
niccit <ntynen+github@gmail.com>
2025-11-30 16:44:31 -1000

    refactored so that the calling class can ask if lights should be put to sleep and returning True/False. Based on the return the caller can then make a call to blank pixels and then sleep. Should be more flexible particularly when multiple light strands are in play

--- led_animations/controlLights.py
+++ led_animations/controlLights.py
@@ -1,31 +1,57 @@
 # SPDX-License-Identifier: MIT
 import alarm
+from alarm import time
 import time
 import supervisor
 
 # Common methods that may be used across projects
 
-# If start up time is before sunset, sleep until set_time
-def sleep_before_set_time(now_time, set_time, before_set_time, ignore_sunset, pixels):
+# Check if lights should be put to sleep until scheduled time
+# Returns True or False
+def check_need_sleep(now_time, set_time, before_set_time, ignore_sunset):
     if now_time < set_time:
         time_diff = (set_time - now_time) - before_set_time
         if 0 < time_diff <= set_time and not ignore_sunset:
-            wake_alarm = alarm.time.TimeAlarm(monotonic_time=time.monotonic() + time_diff)
-            blank_all(pixels)
-            alarm.light_sleep_until_alarms(wake_alarm)
-            supervisor.reload()
+            check = True
+        else:
+            check = False
+        return check
+    else:
+        return False
 
-# If up and running, and it's shut off time, sleep until next day around set_time
+# Check if lights should be put to sleep for a specified amount of time; i.e. next day
+# Returns True or False
+def check_need_shutdown(now_time, stop_time, start_time, sleep_time, before_start_time):
+    running_time = (now_time - start_time)
+    time_diff = (sleep_time - running_time) - before_start_time
+    if stop_time != 0:
+        if now_time >= stop_time and time_diff > 0:
+            check = True
+        else:
+            check = False
+        return check
+    else:
+        return False
+
+# Put lights to sleep until scheduled start time
+# supervisor.reload() is there because lights freeze when coming out of sleep without it
+def sleep_before_set_time(now_time, set_time, before_set_time):
+    time_diff = (set_time - now_time) - before_set_time
+    wake_alarm = alarm.time.TimeAlarm(monotonic_time=time.monotonic() + time_diff)
+    alarm.light_sleep_until_alarms(wake_alarm)
+    supervisor.reload()
+
+# Put lights to sleep for specified amount of time - sleep_time
+# supervisor.reload() is there because lights freeze when coming out of sleep without it
 def shutdown(now_time, stop_time, start_time, sleep_time, before_start_time, pixels):
     running_time = (now_time - start_time)
     time_diff = (sleep_time - running_time) - before_start_time
-    if now_time >= stop_time and time_diff > 0:
-        wake_alarm = alarm.time.TimeAlarm(monotonic_time=time.monotonic() + time_diff)
-        blank_all(pixels)
-        alarm.light_sleep_until_alarms(wake_alarm)
-        supervisor.reload()
+    wake_alarm = alarm.time.TimeAlarm(monotonic_time=time.monotonic() + time_diff)
+    alarm.light_sleep_until_alarms(wake_alarm)
+    supervisor.reload()
 
-# If managing multiple strings, strands can be blanked out before calling sleep and shutdown routines
+# Call method if pixels should be blanked for any reason
+# i.e., sleeping before a specified time or for a specified period of time
 def blank_all(pixels):
     pixels.fill((0, 0, 0))
     pixels.show()
